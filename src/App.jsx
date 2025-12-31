6d228a15559271462f9146f91b7454acb1e6dca0
Gideon Cameron <gideonwork20@gmail.com>
2025-12-31 00:14:22 -0800

    Updated the app.tsx to handle multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
-// import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
 import Show from "./Components/Show";
 import Contact from "./Components/Contact";
 
+import Menu from "./pages/Menu";
+
 const App = () => {
   return (
-    <div className="min-h-screen w-full bg-white">
-      {/* Navigation */}
-      <Navbar />
+    <Router>
+      <div className="min-h-screen w-full bg-white">
+        {/* Navigation */}
+        <Navbar />
 
-      {/* Hero / About Section */}
-      <Hero />
-       <About />
-       <Show />
-      <Contact /> 
-    </div>
+        <Routes>
+          {/* HOME PAGE */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <About />
+                <Show />
+                <Contact />
+              </>
+            }
+          />
+
+          {/* MENU PAGE */}
+          <Route path="/menu" element={<Menu />} />
+
+          {/* HELP PAGE (placeholder for later) */}
+          {/* <Route path="/help" element={<Help />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
